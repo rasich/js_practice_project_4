f26a288be057cd921c582d9000dad09cf58ee891
Nikolay <rasich16@gmail.com>
2021-03-09 19:01:29 +0200

    dynamic create modal

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,6 +1,6 @@
 import $ from '../core';
 
-$.prototype.modal = function() {
+$.prototype.modal = function(created) {
   const scroll = calcScroll();
   
   function calcScroll() {
@@ -25,28 +25,86 @@ $.prototype.modal = function() {
         document.body.style.marginRight = `${scroll}px`;
       }
     });
-  }
-  
-  const closeElements = document.querySelectorAll('[data-close]');
-  closeElements.forEach(elem => {
-    $(elem).click(() => {
-      $('.modal').fadeOut(500);
-      setTimeout(() => {
-        document.body.style.overflow = '';
-        document.body.style.marginRight = `0px`;
-      }, 500);
-    })
-  });
 
-  $('.modal').click( e => {
-    if (e.target.classList.contains('modal')) {
-      $('.modal').fadeOut(500);
-      setTimeout(() => {
-        document.body.style.overflow = '';
-        document.body.style.marginRight = `0px`;
-      }, 500);
-    }
-  });
+    const closeElements = document.querySelectorAll(`${target} [data-close]`);
+    closeElements.forEach(elem => {
+      $(elem).click(() => {
+        $(target).fadeOut(500);
+        setTimeout(() => {
+          document.body.style.overflow = '';
+          document.body.style.marginRight = `0px`;
+        }, 500);
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      });
+    });
+  
+    $(target).click(e => {
+      if (e.target.classList.contains('modal')) {
+        $(target).fadeOut(500);
+        setTimeout(() => {
+          document.body.style.overflow = '';
+          document.body.style.marginRight = `0px`;
+        }, 500);
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      }
+    });
+  }
 };
 
 $('[data-toggle="modal"]').modal();
+
+$.prototype.createModal = function({text, btns} = {}) {
+  for (let i = 0; i < this.length; i++) {
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.setAttribute('id',this[i].getAttribute('data-target').slice(1));
+
+    // btns = {count: num, settings: [ [text, className=[], close, cb] ]}
+    const buttons = [];
+    const {settings} = btns;
+    for (let j = 0; j < btns.count; j++) {
+      let btn = document.createElement('button');
+      btn.classList.add('btn', ...settings[j][1]);
+      btn.textContent = settings[j][0];
+      if (settings[j][2]) {
+        btn.setAttribute('data-close', 'true');
+      }
+      if (settings[j][3] && typeof(settings[j][3]) === 'function') {
+        btn.addEventListener('click', settings[j][3]);
+      }
+
+      buttons.push(btn);
+    }
+
+    modal.innerHTML = `
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <button class="close" data-close>
+            <span>&times;</span>
+          </button>
+
+          <div class="modal-header">
+            <div class="modal-title">
+              ${text.title}
+            </div>
+          </div>
+          <div class="modal-body">
+            ${text.body}
+          </div>
+          <div class="modal-footer">
+            
+          </div>
+        </div>
+      </div>
+    `;
+
+    modal.querySelector('.modal-footer').append(...buttons);
+    document.body.appendChild(modal);
+    $(this[i]).modal(true);
+    $(this[i].getAttribute('data-target')).fadeIn(500);
+  }
+};
